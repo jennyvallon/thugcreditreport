a31ac96325227d71301ca3cd2b8fa1f435cd2712
Jenny Vallon <jennyvallon@yahoo-inc.com>
2016-07-21 00:17:45 -0400

    slightly refactored code. removed extra sessions. app still runs

--- app/controllers/signup.server.controller.js
+++ app/controllers/signup.server.controller.js
@@ -8,14 +8,14 @@ var FileStore = require('session-file-store')(session);
 exports.create = function(req, res, next) {
     var user = new User(req.body);
     
-    req.session.regenerate(function(err) {
-        user.save(function(err) {
+    req.session.regenerate(function(err) {//start a new session
+        user.save(function(err) {//create new user
             if (err) {
                 return next(err);
             } 
             else {
-                req.session.user=user;
-                req.session.save(function(err) {
+                req.session.user=user;//save data to session. "user" represents user data as it would be in database
+                req.session.save(function(err) {//save session
                     if (err) {
                         return next(err);
                     } 
@@ -29,7 +29,7 @@ exports.create = function(req, res, next) {
 
 
 
-//used to send most update questions to the new user
+//populate selfReportingForm object directly from the db
 exports.list2 = function(req, res, questions) {
     Question.find().populate('section').exec(function(err, questions) {
         if (err) {
@@ -42,8 +42,9 @@ exports.list2 = function(req, res, questions) {
 
 //render questions as survey
 exports.render=function(req,res,next){
-    req.session.reload(function(err){
+    req.session.reload(function(err){//load users session
         var questions=JSON.parse(req.session.user.selfReportingForm);//need to do this to make it an array
+       
         if(err){
             next(err);
         }
